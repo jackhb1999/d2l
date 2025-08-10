b5291e6185b6dea938a8635c4c55d6b77cf709cf
fanjx <jack.h.b.fan@outlook.com>
2025-08-10 20:19:25 +0800

    学习llm相关

--- src/llm/mod.rs
+++ src/llm/mod.rs
@@ -1,5 +1,5 @@
-use candle_core::{DType, Device, Result, Var};
-use candle_nn::VarMap;
+use candle_core::{DType, Device, Module, Result, Tensor, Var};
+use candle_nn::{Init, Linear, VarBuilder, VarMap, linear};
 
 fn print_varmap(varmap: &VarMap) {
     let data = varmap.data().lock().unwrap();
@@ -22,7 +22,7 @@ fn varmap_() -> Result<()> {
         // 变量就是可变张量
         let var = Var::randn(0.0f32, 1.0f32, (2, 4), &device)?;
         data.insert("var1".to_string(), var);
-        let var = Var::zeros((4, 3),DType::F32, &device)?;
+        let var = Var::zeros((4, 3), DType::F32, &device)?;
         data.insert("var2".to_string(), var);
     }
     print_varmap(&varmap);
@@ -45,5 +45,73 @@ fn varmap_load() -> Result<()> {
     // 必须具有同等规模才能进行加载
     varmap.load("test.safetensors")?;
     print_varmap(&varmap);
+    Ok(())
+}
+
+#[test]
+fn varbuilder_() -> Result<()> {
+    let device = Device::Cpu;
+    let varmap = VarMap::new();
+    {
+        let mut data = varmap.data().lock().unwrap();
+        let var = Var::zeros((2, 4), DType::F32, &device)?;
+        data.insert("weight".to_string(), var);
+    }
+    // 变量的构建器，用于在指定容器放置变量
+    let mut vb = VarBuilder::from_varmap(&varmap, DType::F32, &device);
+    // 容器中有指定名称的变量就返回，没有就根据hints创建
+    let tensor = vb.get_with_hints(
+        (2, 4),
+        "weight",
+        Init::Randn {
+            mean: 0.,
+            stdev: 1.,
+        },
+    )?;
+    print_varmap(&varmap);
+    // 没有就生成
+    vb.get((1, 4), "bias")?;
+    print_varmap(&varmap);
+    // println!("tensor dims: {:?}", tensor.get_on_dim(1,1).unwrap());
+    Ok(())
+}
+
+struct SimpleModel {
+    linear1: Linear,
+    linear2: Linear,
+    linear3: Linear,
+}
+
+impl SimpleModel {
+    fn new(vb: VarBuilder, in_dim: usize, hidden_dim: usize, out_dim: usize) -> Result<Self> {
+        let linear1 = linear(in_dim, hidden_dim, vb.pp("linear1"))?;
+        let linear2 = linear(hidden_dim, hidden_dim, vb.pp("linear2"))?;
+        let linear3 = linear(hidden_dim, out_dim, vb.pp("linear3"))?;
+        Ok(Self {
+            linear1,
+            linear2,
+            linear3,
+        })
+    }
+
+    fn forward(&self, x: &Tensor) -> Result<Tensor> {
+        let x = self.linear1.forward(x)?;
+        let x = x.relu()?;
+        let x = self.linear2.forward(&x)?;
+        let x = x.relu()?;
+        let x = self.linear3.forward(&x)?;
+        Ok(x)
+    }
+}
+
+#[test]
+fn linear_() -> Result<()> {
+    let device = Device::Cpu;
+    let varmap = VarMap::new();
+    let mut vb = VarBuilder::from_varmap(&varmap, DType::F32, &device);
+    let model = SimpleModel::new(vb, 2, 9, 1)?;
+    print_varmap(&varmap);
+    // let x = Tensor::randn(0.0f32, 1.0f32, (1, 2), &device)?;
+    // let y = model.forward(&x)?;
     Ok(())
 }
